80a5998181b5c2a390508d8c7580081fe69e4ae9
Nishant-here-09 <nishantsingh7499@gmail.com>
2026-02-20 10:19:54 +0530

    Update kth.py

--- kth.py
+++ kth.py
@@ -1,4 +1,4 @@
 class Solution:
     def kthSmallest(self, arr, k):
-        arr.sort()              # Sort the array
-        return arr[k - 1]       # kth smallest element
+        arr.sort()              
+        return arr[k - 1]       
